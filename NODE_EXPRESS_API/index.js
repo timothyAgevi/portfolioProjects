ac05bdd402dbb6f5ada7bf868ebf0365d0070ffb
timothyAgevi <timothyagevi@gmail.com>
2022-02-11 14:55:19 +0300

    initialize bodyParser

--- NODE_EXPRESS_API/index.js
+++ NODE_EXPRESS_API/index.js
@@ -2,4 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser'; //takes in incoming POST request bodies
 
 const app =express() //initailize app
-const PORT =5000;
+const PORT =5000;// define PORT
+
+//initilaze body parser midddleware
+app.use(bodyParser.json())
+//note inn json obj and key enclosed in""
