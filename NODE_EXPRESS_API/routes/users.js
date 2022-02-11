e5616da7953ea57e3a64fe74a93746e1ae96ba76
timothyAgevi <timothyagevi@gmail.com>
2022-02-11 15:53:19 +0300

    create mockup database

--- NODE_EXPRESS_API/routes/users.js
+++ NODE_EXPRESS_API/routes/users.js
@@ -2,6 +2,15 @@ import express from 'express'
 
 const router =express.Router();
 
+const users=[{
+    
+    "firstName":"John",
+    "lastName":"Doe",
+    "age":21
+      
+
+}
+];
 router.get('/users', (req,res)=>{
 res.send('Hello')
 });
